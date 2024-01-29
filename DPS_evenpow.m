a27883a827e9e83de376ec979cde5002df0ae92d
Jean-Guillaume Dumas <Jean-Guillaume.Dumas@imag.fr>
2024-01-29 11:49:42 +0100

    powers of two new version

--- DPS_evenpow.m
+++ DPS_evenpow.m
@@ -4,46 +4,35 @@ if nargin < 3, nmin = 8; end
 
 n = length(A);
 if n ~= 2^( log2(n) )
-error('The matrix dimension must be a power of 2.')
+   error('The matrix dimension must be a power of 2.')
 end
 
 if n <= nmin
-C = A*B;
+   C = A*B;
 else
-m = n/2; i = 1:m; j = m+1:n;
+	m = n/2; i = 1:m; j = m+1:n;
 
-CSTE=A(i,i)/2;
-PA0=-5*CSTE;
-PA6=-5*(CSTE+A(j,i))/2;
-PA4=3*CSTE-2*A(i,j);
-PA1=2*(A(i,i)+A(j,j))-A(i,j)+A(j,i);
-PA2=PA1+PA6;
-PA3=PA1-PA4;
-PA5=PA1+PA0;
-CSTE2=B(j,i)/2;
-PB0=5*CSTE2/2;
-PB1=(4*(B(i,j)+B(j,i))+3*(B(j,j)-B(i,i)))/5;
-PB4=CSTE2+B(j,j);
-PB6=-B(i,i)+CSTE2;
-PB2=PB4-PB1;
-PB5=PB0-PB1;
-PB7=PB1-PB6;
-d1=DPS_evenpow(PA1,PB1,nmin);
-d2=DPS_evenpow(PA2,PB2,nmin);
-d3=DPS_evenpow(PA3,PB0,nmin);
-d4=DPS_evenpow(PA4,PB4,nmin);
-d5=DPS_evenpow(PA5,PB5,nmin);
-d6=DPS_evenpow(PA6,PB6,nmin);
-d7=DPS_evenpow(PA0,PB7,nmin);
-PC1=(d2-d5)/2;
-PC2=-(d4+d7)/2;
-PC3=-2*(d3+d6)/5+PC2;
-c11=2*(-d1+d3-d5-d7)/5;
-PC4=c11/2;
-c21=PC2-PC4;
-c12=c21-PC3;
-c22=PC1+(c21+3*(c12-PC4)/2)/2;
+	t1 = (A(j,i)-A(i,j));
+	t2 = A(j,j)/2;
+	t3 = t1/2;
+	t4 = t2/2-A(i,i);
+	s1 = -(B(i,j)+B(j,i));
+	s2 = -B(i,i)/2;
+	s3 = s1/2;
+	s4 = s2/2-B(j,j);
+	d1 = DPS_evenpow(t1,s1,nmin);
+	d2 = DPS_evenpow((t4-t3),(s2-B(i,j)),nmin);
+	d3 = DPS_evenpow((A(i,j)-t2),(s4-s3),nmin);
+	d4 = DPS_evenpow((t2-A(j,i)),(s2+B(j,i)),nmin);
+	d5 = DPS_evenpow((A(i,j)+t2),(s3+s4),nmin);
+	d6 = DPS_evenpow((t3+t4),(s2+B(i,j)),nmin);
+	d7 = DPS_evenpow((A(j,i)+t2),(B(j,i)-s2),nmin);
+	r8 = (d4-d7)/2-d1;
+	c11 = d2+d6-r8;
+	c12 = d4+d7;
+	c21 = (d2-d3-d5-d6)/2-c12/4;
+	c22 = d3-d5+r8;
 
-C = [ c11 c21 ; c12 c22 ] ;
+	C = [ c11 c21 ; c12 c22 ] ;
 
 end ;
