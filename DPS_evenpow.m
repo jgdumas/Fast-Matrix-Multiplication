18e328270bb5f549b4d97e3344b614b6fbf9b2c3
Jean-Guillaume Dumas <Jean-Guillaume.Dumas@imag.fr>
2024-01-29 15:05:58 +0100

    powers of 2 automatic

--- DPS_evenpow.m
+++ DPS_evenpow.m
@@ -12,27 +12,43 @@ if n <= nmin
 else
 	m = n/2; i = 1:m; j = m+1:n;
 
-	t1 = (A(j,i)-A(i,j));
-	t2 = A(j,j)/2;
-	t3 = t1/2;
-	t4 = t2/2-A(i,i);
-	s1 = -(B(i,j)+B(j,i));
-	s2 = -B(i,i)/2;
-	s3 = s1/2;
-	s4 = s2/2-B(j,j);
-	d1 = DPS_evenpow(t1,s1,nmin);
-	d2 = DPS_evenpow((t4-t3),(s2-B(i,j)),nmin);
-	d3 = DPS_evenpow((A(i,j)-t2),(s4-s3),nmin);
-	d4 = DPS_evenpow((t2-A(j,i)),(s2+B(j,i)),nmin);
-	d5 = DPS_evenpow((A(i,j)+t2),(s3+s4),nmin);
-	d6 = DPS_evenpow((t3+t4),(s2+B(i,j)),nmin);
-	d7 = DPS_evenpow((A(j,i)+t2),(B(j,i)-s2),nmin);
-	r8 = (d4-d7)/2-d1;
-	c11 = d2+d6-r8;
-	c12 = d4+d7;
-	c21 = (d2-d3-d5-d6)/2-c12/4;
-	c22 = d3-d5+r8;
+t1=A(j,j)/2;
+PA2=A(j,i)-t1;
+t3=PA2-A(i,j);
+PA3=A(i,j)-t1;
+PA4=A(j,i)+t1;
+PA5=A(i,i)+t3/2;
+PA0=PA2-PA3;
+PA6=PA4-PA0;
+PA1=PA5-PA0;
 
-	C = [ c11 c21 ; c12 c22 ] ;
+s1=B(i,j)/2;
+PB1=B(i,i)+s1;
+s3=PB1-B(j,j);
+PB2=s1-B(j,j);
+PB3=s3/2-B(j,i);
+PB4=B(j,j)+s1;
+PB0=PB1-PB4;
+PB5=PB0-PB2;
+PB6=PB0-PB3;
+
+	d0 = DPS_evenpow(PA0,PB0,nmin);
+	d1 = DPS_evenpow(PA1,PB1,nmin);
+	d2 = DPS_evenpow(PA2,PB2,nmin);
+	d3 = DPS_evenpow(PA3,PB3,nmin);
+	d4 = DPS_evenpow(PA4,PB4,nmin);
+	d5 = DPS_evenpow(PA5,PB5,nmin);
+	d6 = DPS_evenpow(PA6,PB6,nmin);
+
+r1=d4+d2;
+r2=d4-d2;
+r3=d6+d5-d3+d1;
+r4=d0+r2/2;
+c11=r3/2+r1/4;
+c12=d1-d5+r4;
+c21=d3+d6+r4;
+c22=r1;
+
+	C = [ c11 c12 ; c21 c22 ] ;
 
 end ;
