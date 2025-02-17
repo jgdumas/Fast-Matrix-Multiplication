8f56fc0865f40b8b5a8f97c3398f13b2916cd00a
Jean-Guillaume Dumas <Jean-Guillaume.Dumas@imag.fr>
2025-02-17 16:45:34 +0100

    defensive Incompatible matrix dimensions.

--- FMM-matlab-benchmarks/FMMa.m
+++ FMM-matlab-benchmarks/FMMa.m
@@ -1,13 +1,13 @@
 function C = FMMa(A, B, nmin, peeling, level)
-%          Computes the product C = A*B, with fast core: <6;3;3>.
+%          Computes C = A*B, with fast accurate cores: <6;3;3>,<3;3;6>,<3;6;3>.
 %          nmin   : threshold switch to conventional.
 %          peeling: static peeling (1, by default) or dynamic (2).
 %          level  : logging level.
-if nargin < 3, nmin = 3; end    % Threshold to conventional
-if nargin < 4, peeling = 1; end % Static (1) or Dynamic (2) peeling
-if nargin < 5, level = 3; end   % Verbose level
-  [m,k] = size(A); 
-  [k,n] = size(B);
+  if nargin < 3, nmin = 3; end    % Threshold to conventional
+  if nargin < 4, peeling = 1; end % Static (1) or Dynamic (2) peeling
+  if nargin < 5, level = 3; end   % Verbose level
+  [m,k] = size(A); [k2,n] = size(B);
+  if (k2 ~= k), error('Incompatible matrix dimensions.'); end
   if (m >= max(k,n))
     C=FMMa_6_3_3(A,B,nmin, peeling, level);
   elseif (n >= max(m,k))
