0e55a0891ff68958ce0a2ef125e9bc5ce882ddee
Jean-Guillaume Dumas <Jean-Guillaume.Dumas@imag.fr>
2025-02-17 18:08:08 +0100

    by 6 or 3

--- FMM-matlab-benchmarks/accuracy_54.m
+++ FMM-matlab-benchmarks/accuracy_54.m
@@ -2,32 +2,34 @@
 %	Authors: J-G. Dumas, C. Pernet, A. Sedoglavic
 
 rng(1);
-t = 2;   % Number of points
+t = 5;   % Number of points
 num = 9; % Number of runs for each point
 n_0 = 1; % Basecase of the recursion
 
 E_f = zeros(t,1);E_a = zeros(t,1);E_c = zeros(t,1);E_d = zeros(t,1);
 T_f = zeros(t,1);T_a = zeros(t,1);T_c = zeros(t,1);T_d = zeros(t,1);
-M = zeros(t,1);
-N = zeros(t,1);
+M = zeros(t,1); K = zeros(t,1); N = zeros(t,1);
 
-% A starts with 54x54 and increase (54^l)x(54^l)
-% B starts with 54x54 and increase (54^l)x(54^l)
+% A starts with 6x3 and B starts with 3x3
+
 
 %parfor l = 1:t
 for l = 1:t
-    disp(l);
-    for k = 1:num
-	A = gen_mat_rect(54^l,54^l); M(l) = size(A,1);
-	B = gen_mat_rect(54^l,54^l);   N(l) = size(B,2);
-    disp(M(l));
-    disp(N(l));
+  disp(l);
+  for k = 1:num
+    q=floor(l/3);
+    mu=2^q*3^l; ku=2^q*3^l; nu=2^q*3^l;
+    if (rem(l,3)>=1), mu=mu*2; end
+    if (rem(l,3)>=2), nu=nu*2; end
+	A = gen_mat_rect(mu,ku); M(l) = size(A,1); K(l) = size(A,2);
+	B = gen_mat_rect(ku,nu); N(l) = size(B,2);
+    disp([M(l),K(l),N(l)]);
 	[e_f,e_a,e_c,e_d,t_f,t_a,t_c,t_d] = error_FMM(A,B,n_0,2);
 	E_f(l) = E_f(l) + e_f/num; T_f(l) = T_f(l) + t_f/num;
 	E_a(l) = E_a(l) + e_a/num; T_a(l) = T_f(l) + t_f/num;
 	E_c(l) = E_c(l) + e_c/num; T_c(l) = T_c(l) + t_c/num;
 	E_d(l) = E_d(l) + e_d/num; T_d(l) = T_d(l) + t_d/num;
-    end
+  end
 end
 
 save('accuracy_54','M','N','E_f','E_a','E_c','E_d');
