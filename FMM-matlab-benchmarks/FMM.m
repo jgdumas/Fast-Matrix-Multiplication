db0e20e88a44704656adfba455dccac7d544ce35
Jean-Guillaume Dumas <Jean-Guillaume.Dumas@imag.fr>
2025-02-24 19:40:45 +0100

    3 3 6 recursive cutting

--- FMM-matlab-benchmarks/FMM.m
+++ FMM-matlab-benchmarks/FMM.m
@@ -1,18 +1,16 @@
 function C = FMM(A, B, nmin, peeling, level)
-%          Computes the product C = A*B, with fast cores <6;3;3>,<3;3;6>,<3;6;3>.
-%          nmin   : threshold switch to conventional.
-%          peeling: static peeling (1, by default) or dynamic (2).
-%          level  : logging level.
-  if nargin < 3, nmin = 3; end    % Threshold to conventional
+%          Computes the product C = A*B,
+%          via <3;3;6>, <3;6;3> or <6;3;3> fast algorithms.
+  if nargin < 3, nmin = 4; end    % Threshold to conventional
   if nargin < 4, peeling = 1; end % Static (1) or Dynamic (2) peeling
-  if nargin < 5, level = 3; end   % Verbose level
-  [m,k] = size(A); [k2,n] = size(B);
-  if (k2 ~= k), error('Incompatible matrix dimensions.'); end
+  if nargin < 5, level = 8; end   % Verbose level
+  [m,k] = size(A);
+  [k,n] = size(B);
   if (m >= max(k,n))
-    C=FMM_6_3_3(A,B,nmin, peeling, level);
+    C=FMM_6_3_3(A, B, nmin, peeling, level);
   elseif (n >= max(m,k))
-    C=FMM_3_3_6(A,B,nmin, peeling, level);
+    C=FMM_3_3_6(A, B, nmin, peeling, level);
   else
-    C=FMM_3_6_3(A,B,nmin, peeling, level);
+    C=FMM_3_6_3(A, B, nmin, peeling, level);
   end
 end
