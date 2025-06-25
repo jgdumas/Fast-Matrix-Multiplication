d39272a1b35acbb70dc72e96d2b92cc3dbd92c86
Jean-Guillaume Dumas <Jean-Guillaume.Dumas@imag.fr>
2025-06-25 18:01:42 +0200

    functions 4 program generation

---
+++ FMM-plinopt-codegen/functions4sms.sh
@@ -0,0 +1,233 @@
+#!/bin/bash
+# ==========================================================================
+# Fast-Matrix-Multiplication library
+#   https://github.com/jgdumas/Fast-Matrix-Multiplication
+#   Accurate fast matrix multiplications via recursion
+# ==========================================================================
+# function4sms: tools for matlab/maple program generation
+# ==========================================================================
+#   Authors:
+#   [J-G. Dumas, C. Pernet, A. Sedoglavic;
+#    Strassen's algorithm is not optimally accurate;
+#    ISSAC 2024, Raleigh, NC USA, pp. 254-263.
+#    https://hal.science/hal-04441653]
+# ==========================================================================
+
+# ==========================================================================
+##########
+# Function: Matrix Multiplication Check
+#
+function MMcheck {
+    local Lsms=$1
+    local Rsms=$2
+    local Psms=$3
+    local SQRT=$4
+    local PLACE=$5
+
+    local MMFLAGS=""
+    if [[ "$SQRT" -ne 0 ]]; then
+	local MMFLAGS="128 ${SQRT} ${PLACE}"
+    fi
+    echo "MMchecker ${Lsms} ${Rsms} ${Psms} ${MMFLAGS} "
+    local mkn=`MMchecker ${Lsms} ${Rsms} ${Psms} ${MMFLAGS} |& grep '#'`
+    echo $mkn
+    if [[ "$mkn" == *"ERROR"* ]]; then
+	exit 1;
+    fi
+}
+# ==========================================================================
+
+# ==========================================================================
+##########
+# Function: Generator of SLPs from 3 (2 direct and 1 transposed) sms matrices
+#
+function sms2slp {
+    local Lmat=$1
+    local Rmat=$2
+    local Pmat=$3
+    local MMCHECK=$4
+    local SQRT=$5
+    local PLACE=$6
+
+    local Lsms=${Lmat}.sms
+    local Rsms=${Rmat}.sms
+    local Psms=${Pmat}.sms
+
+    local Lslp=${Lmat}.slp
+    local Rslp=${Rmat}.slp
+    local Pslp=${Pmat}.slp
+
+    ##########
+    # Do represent a matrix multiplication algorithm
+    #
+    if [[ "$MMCHECK" -eq 1 ]]; then
+	MMcheck ${Lsms} ${Rsms} ${Psms} ${SQRT} ${PLACE}
+    else
+	echo "<$m;$k;$n> algorithm of rank $r."
+    fi
+
+    ##########
+    # Computation of the straight-line programs
+    #
+    for mat in $Lmat $Rmat
+      do
+      local Msms=${mat}.sms
+      local Mslp=${mat}.slp
+      echo "Generating ${Mslp} with flags: ${OPTFLAGS}"
+      optimizer ${OPTFLAGS} ${Msms} | compacter -s > ${Mslp}
+    done
+
+    echo "Generating ${Pslp}, by transposition, with flags: ${OPTFLAGS}"
+    matrix-transpose ${Psms} | optimizer ${OPTFLAGS} | transpozer | compacter -s > ${Pslp}
+
+
+    ##########
+    # Verifications
+    #
+    for mat in $Lmat $Rmat $Pmat
+      do
+      local Msms=${mat}.sms
+      local Mslp=${mat}.slp
+      local pmc=`SLPchecker -M ${Msms} ${Mslp} |& grep '#'`
+      echo $pmc
+      if [[ "$pmc" == *"ERROR"* ]]; then
+	  exit 1;
+      fi
+    done
+}
+# ==========================================================================
+
+# ==========================================================================
+##########
+# Function: replacing the placeholder by sqrt, with precomputed constants
+#
+function PlaceHolder {
+    local SQRT=$1
+    local PLACE=$2
+    shift 2
+    if [[ "$SQRT" -ne 0 ]]; then
+	DBLP=$(( ${PLACE} * 2 ))
+	sed -i "s/${DBLP}/2\*${PLACE}/g;s/${PLACE}/sqrt(${SQRT})/g;s/sqrt(${SQRT})\*2\/3/SQRT${SQRT}f2o3/g;s/2\*sqrt(${SQRT})\/3/SQRT${SQRT}f2o3/g;s/sqrt(${SQRT})\/2/SQRT${SQRT}o2/g;s/sqrt(${SQRT})\/3/SQRT${SQRT}o3/g;s/function .*/&\nSQRT${SQRT}o2=sqrt(${SQRT})\/2;\nSQRT${SQRT}o3=sqrt(${SQRT})\/3;\nSQRT${SQRT}f2o3=sqrt(${SQRT})\*2\/3;\n/" $*
+    fi
+}
+# ==========================================================================
+
+# ==========================================================================
+##########
+# Function: Gathering of all CoB SLPs to produce a Matlab program
+#
+function slp2CBm {
+    local Lslp=$1
+    local Rslp=$2
+    local Pslp=$3
+    local m=$4
+    local k=$5
+    local n=$6
+    local r=$7
+    local File=$8
+    echo "Generating ${File}_CoBL.m ${File}_CoBR.m ${File}_ICoB.m change of bases:"
+    ./CoB.rpl ${Lslp} ${m} ${k} ${File}_CoBL
+    ./CoB.rpl ${Rslp} ${k} ${n} ${File}_CoBR
+    ./CoB.rpl ${Pslp} ${m} ${n} ${File}_ICoB
+
+    PlaceHolder ${SQRT} ${PLACE} ${File}_CoBL.m ${File}_CoBR.m ${File}_ICoB.m
+}
+# ==========================================================================
+
+
+# ==========================================================================
+##########
+# Function: Gathering of SLPs to form a Matlab program
+#
+function slp2MMm {
+    local Lslp=$1
+    local Rslp=$2
+    local Pslp=$3
+    local m=$4
+    local k=$5
+    local n=$6
+    local r=$7
+    local File=$8
+    echo "Generating ${File}.m with ${m}x${k}x${n} of rank ${r}:"
+    ./MM.rpl ${Lslp} ${Rslp} ${Pslp} ${m} ${k} ${n} ${r} ${File} 1
+    PlaceHolder ${SQRT} ${PLACE} ${File}_${m}_${k}_${n}.m
+}
+# ==========================================================================
+
+
+# ==========================================================================
+##########
+# Function: combine two squential programs to check with linear operator
+#
+function combPMcheck {
+    local mat=$1
+    local pri=$2
+    local pro=$3
+    local usedvar=`sed 's/[^[:alpha:]]//g' ${pri} ${pro} | grep -o . | sort -u | tr -d "\n"`
+    local freechar=`echo "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" | sed "s/${usedvar}//g" | grep -o . | head -1`
+    sed -r "s/(o)([0-9]*)/${freechar}\2/g" ${pri} > s2m.slp
+    sed -r "s/(i)([0-9]*)/${freechar}\2/g" ${pro} >> s2m.slp
+
+    local pmc=`SLPchecker -M ${mat} s2m.slp |& grep '#'`
+    echo $pmc
+    if [[ "$pmc" == *"ERROR"* ]]; then
+	echo "Combined ${pri} ${pro}: SLPchecker -M ${mat} s2m.slp"
+	exit 1;
+    else
+	\rm s2m.slp
+    fi
+}
+# ==========================================================================
+
+
+
+# ==========================================================================
+##########
+# Function: Gathering of SLPs to form a Maple program
+#
+function slp2MMmpl {
+    local Lslp=$1
+    local Rslp=$2
+    local Pslp=$3
+    local m=$4
+    local k=$5
+    local n=$6
+    local r=$7
+    local suffix=$8
+
+    filename="${m}x${k}x${n}_${r}_${suffix}.mpl"
+
+    echo "# Left SLP" > ${filename}
+
+echo "    (./replacer ${Lslp} -M i o A 3 3 40 1) >> ${filename} "
+    (./replacer ${Lslp} -M i o A ${m} ${k} ${r} 1) >> ${filename}
+
+    echo "# Right SLP" >> ${filename}
+    (./replacer ${Rslp} -M i o B ${k} ${n} ${r} 1) >> ${filename}
+
+    rmun=$((r-1))
+    echo "# Inner products: ${rmun}" >> ${filename}
+    for i in $(seq 0 ${rmun})
+      do
+      (echo -n "iC$i:=oA$i * oB$i; ") >> ${filename}
+    done
+    echo  >> ${filename}
+
+    echo "# Post SLP" >> ${filename}
+    (./replacer ${Pslp} -M i o C ${m} ${n} ${r} 0) >> ${filename}
+
+    echo "# Check" >> ${filename}
+    echo "Errors := LinearAlgebra:-Map(expand, C - ((Matrix(${m}, ${k}, symbol = 'A')) . (Matrix(${k}, ${n}, symbol = 'B')))); NumErrors:=LinearAlgebra:-Rank(Errors);" >> ${filename}
+
+    if [ "${MAPLEHERE}" = true ] ; then
+	tmpfile=/tmp/fdt_s2m.$$
+	cat ${filename} | ${MAPLEPROG} | tee ${tmpfile} | tail
+	success=`grep "NumErrors := 0" ${tmpfile} | wc -l`
+	if [[ "${success}" -ne 1 ]]; then
+	    echo -e "\033[1;31m**** ERRORS in: cat ${filename} | ${MAPLEPROG} ***\033[0m"
+	else
+	   echo -e "\033[1;32mSUCCESS: ${filename} is a MM algorithm.\033[0m"
+	fi
+    fi
+}
+# ==========================================================================
--- FMM-plinopt-codegen/sms2maple.sh
+++ FMM-plinopt-codegen/sms2maple.sh
@@ -14,7 +14,14 @@
 # ==========================================================================
 
 
+# ==========================================================================
+##########
+# functions for program generation
+#
+source functions4sms.sh
 
+
+# ==========================================================================
 ##########
 # L,R,P matrices
 #
@@ -41,9 +48,11 @@ m=$(( ${Pd[0]} / ${n} ))
 k=$(( ${Rd[1]} / ${n} ))
 
 r=`grep -v '#' ${Lsms} | head -1 | cut -d' ' -f 1`
-# ==========================================================================
 
 
+##########
+# test for maple program
+#
 MAPLEPROG=maple
 MAPLEHERE=true
 if ! command -v maple &> /dev/null
@@ -53,10 +62,10 @@ then
 else
     if ! echo "1234+5678;" | maple | grep 6912 &> /dev/null
     then
-        echo "`command -v maple` is here but not running"
-        MAPLEHERE=false
+	echo "`command -v maple` is here but not running"
+	MAPLEHERE=false
     else
-        echo "maple is up and running"
+	echo "maple is up and running"
     fi
 fi
 if [ "${MAPLEHERE}" != true ] ; then
@@ -64,176 +73,42 @@ if [ "${MAPLEHERE}" != true ] ; then
 fi
 
 
-
-
 # ==========================================================================
-##########
-# Function: Matrix Multiplication Check
+# Check MM
 #
-function MMcheck {
-    local Lsms=$1
-    local Rsms=$2
-    local Psms=$3
-    local SQRT=$4
-    local PLACE=$5
-
-    local MMFLAGS=""
-    if [[ "$SQRT" -ne 0 ]]; then
-	local MMFLAGS="128 ${SQRT} ${PLACE}"
-    fi
-    echo "MMchecker ${Lsms} ${Rsms} ${Psms} ${MMFLAGS} "
-    local mkn=`MMchecker ${Lsms} ${Rsms} ${Psms} ${MMFLAGS} |& grep '#'`
-    echo $mkn
-    if [[ "$mkn" == *"ERROR"* ]]; then
-	exit 1;
-    fi
-}
-# ==========================================================================
-
-# ==========================================================================
-##########
-# Function: Generator of SLPs from 3 (2 direct and 1 transposed) sms matrices
-#
-function sms2slp {
-    local Lmat=$1
-    local Rmat=$2
-    local Pmat=$3
-    local MMCHECK=$4
-    local SQRT=$5
-    local PLACE=$6
-
-    local Lsms=${Lmat}.sms
-    local Rsms=${Rmat}.sms
-    local Psms=${Pmat}.sms
-
-    local Lslp=${Lmat}.slp
-    local Rslp=${Rmat}.slp
-    local Pslp=${Pmat}.slp
-
-    ##########
-    # Do represent a matrix multiplication algorithm
-    #
-    if [[ "$MMCHECK" -eq 1 ]]; then
-	MMcheck ${Lsms} ${Rsms} ${Psms} ${SQRT} ${PLACE}
-    else
-	echo "<$m;$k;$n> algorithm of rank $r."
-    fi
-
-    ##########
-    # Computation of the straight-line programs
-    #
-    for mat in $Lmat $Rmat
-      do
-      local Msms=${mat}.sms
-      local Mslp=${mat}.slp
-      echo "Generating ${Mslp} with flags: ${OPTFLAGS}"
-      optimizer ${OPTFLAGS} ${Msms} | compacter -s > ${Mslp}
-    done
-
-    echo "Generating ${Pslp}, by transposition, with flags: ${OPTFLAGS}"
-    matrix-transpose ${Psms} | optimizer ${OPTFLAGS} | transpozer | compacter -s > ${Pslp}
-
-
-    ##########
-    # Verifications
-    #
-    for mat in $Lmat $Rmat $Pmat
-      do
-      local Msms=${mat}.sms
-      local Mslp=${mat}.slp
-      local pmc=`PMchecker -M ${Msms} ${Mslp} |& grep '#'`
-      echo $pmc
-      if [[ "$pmc" == *"ERROR"* ]]; then
-	  exit 1;
-      fi
-    done
-}
-# ==========================================================================
-
-# ==========================================================================
-##########
-# Function: replacing the placeholder by sqrt, with precomputed constants
-#
-function PlaceHolder {
-    local SQRT=$1
-    local PLACE=$2
-    shift 2
-    if [[ "$SQRT" -ne 0 ]]; then
-	DBLP=$(( ${PLACE} * 2 ))
-	sed -i "s/${DBLP}/2\*${PLACE}/g;s/${PLACE}/sqrt(${SQRT})/g;s/sqrt(${SQRT})\*2\/3/SQRT${SQRT}f2o3/g;s/2\*sqrt(${SQRT})\/3/SQRT${SQRT}f2o3/g;s/sqrt(${SQRT})\/2/SQRT${SQRT}o2/g;s/sqrt(${SQRT})\/3/SQRT${SQRT}o3/g;s/function .*/&\nSQRT${SQRT}o2=sqrt(${SQRT})\/2;\nSQRT${SQRT}o3=sqrt(${SQRT})\/3;\nSQRT${SQRT}f2o3=sqrt(${SQRT})\*2\/3;\n/" $*
-    fi
-}
-# ==========================================================================
-
-
-
-
-# ==========================================================================
-##########
-# Function: 
-#
-function slp2MMmpl {
-    local Lslp=$1
-    local Rslp=$2
-    local Pslp=$3
-    local m=$4
-    local k=$5
-    local n=$6
-    local r=$7
-    local suffix=$8
-
-    filename="${m}x${k}x${n}_${r}_${suffix}.mpl"
-
-    echo "# Left SLP" > ${filename}
-
-echo "    (./replacer ${Lslp} -M i o A 3 3 40 1) >> ${filename} "
-    (./replacer ${Lslp} -M i o A ${m} ${k} ${r} 1) >> ${filename}
-
-    echo "# Right SLP" >> ${filename}
-    (./replacer ${Rslp} -M i o B ${k} ${n} ${r} 1) >> ${filename}
-    
-    rmun=$((r-1))
-    echo "# Inner products: ${rmun}" >> ${filename}
-    for i in $(seq 0 ${rmun})
-      do
-      (echo -n "iC$i:=oA$i * oB$i; ") >> ${filename}
-    done
-    echo  >> ${filename}
-    
-    echo "# Post SLP" >> ${filename}
-    (./replacer ${Pslp} -M i o C ${m} ${n} ${r} 0) >> ${filename}
-    
-    echo "# Check" >> ${filename}
-    echo "Errors := LinearAlgebra:-Map(expand, C - ((Matrix(${m}, ${k}, symbol = 'A')) . (Matrix(${k}, ${n}, symbol = 'B')))); NumErrors:=LinearAlgebra:-Rank(Errors);" >> ${filename}
-
-    if [ "${MAPLEHERE}" = true ] ; then
-	tmpfile=/tmp/fdt_s2m.$$
-	cat ${filename} | ${MAPLEPROG} | tee ${tmpfile}
-	success=`grep "NumErrors := 0" ${tmpfile} | wc -l`
-	if [[ "${success}" -ne 1 ]]; then
-	    echo -e "\033[1;31m**** ERRORS in: cat ${filename} | ${MAPLEPROG} ***\033[0m"
-	else
-	   echo -e "\033[1;32mSUCCESS: ${filename} is a MM algorithm.\033[0m"
-	fi
-    fi
-}
-
-
-
-
 MMcheck ${Lsms} ${Rsms} ${Psms} ${SQRT} ${PLACE}
 
-BUILDSLP=0
 
-if [[ "$BUILDSLP" -eq 1 ]]; then
+# ==========================================================================
+# Get associated SLPs
+#
+Lslp=${Lmat}.slp
+Rslp=${Rmat}.slp
+Pslp=${Pmat}.slp
+BSLP=0
+
+if [ -f ${Lslp} ] || [ -f ${Rslp} ] || [ -f ${Lslp} ]; then
+    read -p "Overwrite ${Lslp}, ${Rslp} and ${Pslp}? [y/N]" RESP
+    if [[ "$RESP" -eq "y" ]]; then
+	BSLP=1
+    fi
+else
+    BSLP=1
+fi
+
+if [[ "$BSLP" -eq 1 ]]; then
 	# Do generate the SLPs:
     sms2slp ${Lmat} ${Rmat} ${Pmat} ${MMCHECK} ${SQRT} ${PLACE}
 fi
 
-Lslp=${Lmat}.slp
-Rslp=${Rmat}.slp
-Pslp=${Pmat}.slp
 
 
-	# Bilinear algorithm for matrix multiplication only
+# ==========================================================================
+# Bilinear algorithm for matrix multiplication only
+#
 slp2MMmpl ${Lslp} ${Rslp} ${Pslp} ${m} ${k} ${n} ${r} ${File}
+
+
+
+# ==========================================================================
+# ==========================================================================
